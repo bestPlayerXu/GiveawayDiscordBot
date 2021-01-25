d08341df291e8712d51d906b8168630dc684fc0e
bestPlayer_xu <max.mingxu@gmail.com>
2021-01-25 08:50:35 +0100

    Added stop script and basic command parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+
 ///////PROCESS HANDLER/////////////
 
 process.title = 'GiveawayBot';
+fs.writeFileSync('logs/pid.log', '' + process.pid);
+
 //not using the process.on('exit') to be able to call async functions
 var exit = () => {
 	//do whatever necessary before exit
@@ -18,20 +22,40 @@ var client = new (require('discord.js').Client)();
 client.on('ready', () => {
 	console.log('Logged in as ' + client.user.tag + '.')
 	
-	//Slash commands:
-	console.log(client.api.applications(client.user.id).commands.get());
+	/////////Slash commands:///////////////
+	//Init
 	client.api.applications(client.user.id).guilds('776876439671668826').commands.post({
 		data: {
 			name: 'Test',
 			description: 'test command'
 		}
 	});
+	//Inveraction
+	client.ws.on('INTERACTION_CREATE', interaction => {
+        var command = interaction.data.name.toLowerCase();
+        var channel = client.channels.get(interaction.channel_id);
+        
+		console.log('Slashing ' + command + ' in ' + channel.name);
+		
+		client.api.interactions(interaction.id, interaction.token).callback.post({
+			data: {
+				type: 4,
+				data: {
+					content: 'Executing ' + command + ' as if you typed it in chat ...'
+				}
+			}
+		});
+    });
 });
 
 ////////CORE//////////////////
 client.on('message', msg => {
 	if (msg.author.bot) return;
-	msg.reply('Bot online.');
+	var prefix;
+	if (msg.content.startsWith('<@!' + client.user.id + '>')) prefix = msg.content.slice(0, ('<@!' + client.user.id + '>').length); //ping
+	if (msg.content.startsWith('ga!')) prefix = 'ga!'; //prefix
+	if (!prefix) return; //nothing for me
+	var command = msg.content.split(prefix)[1].split(' ').filter(s => s.length)[0];
 });
 
 client.login(process.env.BOT_TOKEN);
